3b2dc6b36e40546030c7d9e7d0d15b8f8a1c8fcd
Jojjer123 <70746514+Jojjer123@users.noreply.github.com>
2022-02-28 18:56:17 +0100

    Change variable names & add command detection.
    Add command detection on the channel for communication with the admin interface.

--- pkg/deviceManager/deviceManager.go
+++ pkg/deviceManager/deviceManager.go
@@ -7,25 +7,42 @@ import (
 
 const maxNumberOfDeviceMonitors = 10
 
-func DeviceManager(waitGroup *sync.WaitGroup, deviceChannel <-chan string) {
+func DeviceManager(waitGroup *sync.WaitGroup, adminChannel <-chan string) {
 	fmt.Println("DeviceManager started")
 	defer waitGroup.Done()
 
+	var deviceMonitorWaitGroup sync.WaitGroup
+	managerChannel := make(chan string)
 	numberOfDeviceMonitors := 0
 
-	i := 0
-	for {
-		if i == 0 && numberOfDeviceMonitors < maxNumberOfDeviceMonitors {
-			createDeviceMonitor(&numberOfDeviceMonitors, deviceChannel)
+	deviceManagerIsActive := true
+	for deviceManagerIsActive {
+		select {
+		case msg := <-adminChannel:
+			if msg == "shutdown" {
+				fmt.Println("Device manager received shutdown command")
+				// deviceManagerIsActive = false
+				for i := 0; i < numberOfDeviceMonitors; i++ {
+					managerChannel <- msg
+				}
+			} else if msg == "create new" {
+				fmt.Println("Device manager received create new command")
+				if numberOfDeviceMonitors < maxNumberOfDeviceMonitors {
+					fmt.Println("Create new device monitor...")
+					createDeviceMonitor(&numberOfDeviceMonitors, managerChannel, &deviceMonitorWaitGroup)
+				}
+			}
 		}
-
-		i++
 	}
+
+	deviceMonitorWaitGroup.Wait()
+	fmt.Println("Device manager shutting down...")
 }
 
-func createDeviceMonitor(numberOfDeviceMonitors *int, deviceChannel <-chan string) {
+func createDeviceMonitor(numberOfDeviceMonitors *int, managerChannel <-chan string, deviceMonitorWaitGroup *sync.WaitGroup) {
 	*numberOfDeviceMonitors += 1
+	deviceMonitorWaitGroup.Add(1)
 
 	config := "test-configuration"
-	go deviceMonitor(config, numberOfDeviceMonitors, deviceChannel)
+	go deviceMonitor(config, numberOfDeviceMonitors, managerChannel, deviceMonitorWaitGroup)
 }
