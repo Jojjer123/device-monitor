951172e0bdd3e65c476fd158e85e69462bb0b953
Jojjer123 <70746514+Jojjer123@users.noreply.github.com>
2022-03-02 11:46:31 +0100

    Rename file

--- pkg/admin/generated/adminServer.go
+++ pkg/admin/generated/adminServer.go
@@ -9,8 +9,6 @@ import (
 type Server struct {
 }
 
-// Rename this file to adminServer.go
-
 func (s *Server) MonitorDevice(ctx context.Context, message *MonitorMessage) (*MonitorResponse, error) {
 	fmt.Println("Received message from client: ", message.Action, " ", message.Target)
 	return &MonitorResponse{Response: "Successfully created monitor"}, nil
