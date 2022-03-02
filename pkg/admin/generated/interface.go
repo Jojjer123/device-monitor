9336910f31128d297c703bf93e763e9526e2ce80
Jojjer123 <70746514+Jojjer123@users.noreply.github.com>
2022-03-02 11:05:26 +0100

    Correct spelling & add prints for gprc server

--- pkg/admin/generated/interface.go
+++ pkg/admin/generated/interface.go
@@ -1,12 +1,17 @@
 package adminServer
 
 import (
+	"fmt"
+
 	"golang.org/x/net/context"
 )
 
 type Server struct {
 }
 
+// Rename this file to adminServer.go
+
 func (s *Server) MonitorDevice(ctx context.Context, message *MonitorMessage) (*MonitorResponse, error) {
-	return &MonitorResponse{Respone: "Successfully created monitor"}, nil
+	fmt.Println("Received message from client: ", message.Action, " ", message.Target)
+	return &MonitorResponse{Response: "Successfully created monitor"}, nil
 }
