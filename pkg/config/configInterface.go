e88897dd981488449589d7ab1dca18a9ebb86701
Jojjer123 <70746514+Jojjer123@users.noreply.github.com>
2022-03-10 16:22:41 +0100

    Minor print changes & changed TODO

--- pkg/config/configInterface.go
+++ pkg/config/configInterface.go
@@ -19,16 +19,16 @@ func ConfigInterface(waitGroup *sync.WaitGroup) {
 	fmt.Println("ConfigInterface started")
 	defer waitGroup.Done()
 
-	// TODO: Send gnmi request for the config from the storage service
+	// TODO: Implement cert usage
 
-	var c client.Impl
-	var err error
+	// var c client.Impl
+	// var err error
 
 	ctx := context.Background()
 
 	address := []string{"storage-service:11161"}
 
-	c, err = gclient.New(ctx, client.Destination{
+	c, err := gclient.New(ctx, client.Destination{
 		Addrs:       address,
 		Target:      "storage-service",
 		Timeout:     time.Second * 5,
@@ -37,7 +37,9 @@ func ConfigInterface(waitGroup *sync.WaitGroup) {
 	})
 
 	if err != nil {
-		fmt.Errorf("could not create a gNMI client: %v", err)
+		// fmt.Errorf("could not create a gNMI client: %v", err)
+		fmt.Print("Could not create a gNMI client: ")
+		fmt.Println(err)
 	}
 
 	// "path: <target: 'storage-service', elem: <name: 'interfaces'> elem: <name: 'interface' key: <key: 'name' value: 'sw0p5'>>>"
@@ -45,13 +47,19 @@ func ConfigInterface(waitGroup *sync.WaitGroup) {
 	r := &gpb.GetRequest{}
 
 	if err := proto.UnmarshalText("path: <target: 'storage-service', elem: <name: 'interfaces'> elem: <name: 'interface' key: <key: 'name' value: 'sw0p5'>>>", r); err != nil {
-		fmt.Errorf("unable to parse gnmi.GetRequest: %v", err)
+		// fmt.Errorf("unable to parse gnmi.GetRequest: %v", err)
+		fmt.Print("Unable to parse gnmi.GetRequest: ")
+		fmt.Println(err)
 	}
 
 	response, err := c.(*gclient.Client).Get(ctx, r)
 
 	if err != nil {
-		fmt.Errorf("target returned RPC error for Get(%q): %v", r.String(), err)
+		// fmt.Errorf("target returned RPC error for Get(%q): %v", r.String(), err)
+		fmt.Print("Target returned RPC error for Get(")
+		fmt.Print(r.String())
+		fmt.Print("): ")
+		fmt.Println(err)
 	}
 
 	var config types.Config
