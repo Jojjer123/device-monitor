971365b6506d5637856d084c6a350c50962ef87f
Jojjer123 <70746514+Jojjer123@users.noreply.github.com>
2022-03-11 12:54:03 +0100

    Add subscribe functionality.

--- pkg/northbound/subscribe.go
+++ pkg/northbound/subscribe.go
@@ -2,6 +2,7 @@ package northboundInterface
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/google/gnxi/utils/credentials"
 	"google.golang.org/grpc/codes"
@@ -31,6 +32,41 @@ func (s *server) Subscribe(stream pb.GNMI_SubscribeServer) error {
 	}
 
 	fmt.Println(message)
+
+	// upd := []*pb.Update{
+	// 	Value: &pb.Value{
+	// 		Value: []byte("Hello from the other side"),
+	// 	},
+	// }
+
+	// var notification *pb.Notification
+
+	// notification.Update[0].Value.Value = []byte("test")
+
+	var upd []*pb.Update
+
+	fuckingHell := pb.TypedValue_StringVal{
+		StringVal: "from the other fucking side",
+	}
+
+	reee := pb.Update{
+		Val: &pb.TypedValue{
+			Value: &fuckingHell,
+		},
+	}
+
+	upd = append(upd, &reee)
+
+	response := pb.SubscribeResponse{
+		Response: &pb.SubscribeResponse_Update{
+			Update: &pb.Notification{
+				Update: upd,
+			},
+		},
+	}
+	stream.Send(&response)
+	time.Sleep(5 * time.Second)
+	stream.Send(&response)
 
 	return nil //s.Server.Subscribe(stream)
 }
