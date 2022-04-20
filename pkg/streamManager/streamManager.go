29fa6abe5478ec30217f1b9cacdd6d15c321467a
Jojjer123 <70746514+Jojjer123@users.noreply.github.com>
2022-04-20 12:09:35 +0200

    Add debuggning.

--- pkg/streamManager/streamManager.go
+++ pkg/streamManager/streamManager.go
@@ -11,20 +11,25 @@ import (
 // var streamStore []types.Stream
 
 func StreamManager(waitGroup *sync.WaitGroup, streamMgrChannel chan types.StreamMgrChannelMessage) { //, adminChannel chan types.AdminChannelMessage) {
+	fmt.Println("Started StreamManager")
 	defer waitGroup.Done()
 
 	// TODO: Remove streamWaitGroup and add better way of keeping module "alive".
 
 	var streamWaitGroup sync.WaitGroup
 
+	fmt.Println("Going to send function from StreamManager")
 	var streamMgrMessage types.StreamMgrChannelMessage
 	streamMgrMessage.ManageCmd = streamMgrCmd
 	streamMgrChannel <- streamMgrMessage
+	fmt.Println("Sent function from StreamManager")
 
 	streamWaitGroup.Wait()
+	fmt.Println("Closed StreamManager")
 }
 
 func streamMgrCmd(stream types.Stream, cmd string) string {
+	fmt.Println("Cmd arrived to StreamManager")
 	fmt.Println(stream.Target, cmd)
 
 	return ""
