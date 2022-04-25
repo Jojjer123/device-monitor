cb5a71d017230ec6ca286ec9661aa8218ae7e2b1
Jojjer123 <70746514+Jojjer123@users.noreply.github.com>
2022-04-25 15:26:01 +0200

    Change wire format.

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -74,10 +74,10 @@ func (m *AdapterResponse) String() string { return proto.CompactTextString(m) }
 func (m *AdapterResponse) ProtoMessage()  {}
 
 type SchemaEntry struct {
-	NameIdentifier string `protobuf:"bytes,2,opt,name=NameIdentifier,proto3"`
-	Tag            string `protobuf:"bytes,2,opt,name=Tag,proto3"`
-	Namespace      string `protobuf:"bytes,2,opt,name=Namespace,proto3"`
-	Value          string `protobuf:"bytes,2,opt,name=Value,proto3"`
+	Name      string `protobuf:"bytes,2,opt,name=NameIdentifier,proto3"`
+	Tag       string `protobuf:"bytes,2,opt,name=Tag,proto3"`
+	Namespace string `protobuf:"bytes,2,opt,name=Namespace,proto3"`
+	Value     string `protobuf:"bytes,2,opt,name=Value,proto3"`
 }
 
 type GatewayData struct {
