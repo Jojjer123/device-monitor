260fea8c07b2860801165c7c00aae5d9a38137b0
Jojjer123 <70746514+Jojjer123@users.noreply.github.com>
2022-05-16 14:53:31 +0200

    Revert check before unmarshaling.

--- pkg/configManager/deviceMonitor.go
+++ pkg/configManager/deviceMonitor.go
@@ -143,7 +143,7 @@ func extractData(response *gnmi.GetResponse, req *gnmi.GetRequest, name string) 
 
 	// fmt.Printf("Response: %v", response)
 
-	if len(response.Notification[0].Update) > 0 {
+	if len(response.Notification) > 0 {
 		// fmt.Printf("Response: %v\n", response)
 		// fmt.Println("------------------------------")
 		if err := proto.Unmarshal(response.Notification[0].Update[0].Val.GetProtoBytes(), &adapterResponse); err != nil {
